2cf5c94b6246c7b0cd90e64a61bc068cef7ca082
Jared Stewart <jstewart@pivotal.io>
2017-06-20 10:32:46 -0700

    GEODE-3097: GFSH works over HTTP with SSL

--- geode-core/src/test/java/org/apache/geode/management/ConnectToLocatorSSLDUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/management/ConnectToLocatorSSLDUnitTest.java
@@ -33,8 +33,23 @@ import static org.apache.geode.distributed.ConfigurationProperties.SSL_KEYSTORE_
 import static org.apache.geode.distributed.ConfigurationProperties.SSL_PROTOCOLS;
 import static org.apache.geode.distributed.ConfigurationProperties.SSL_TRUSTSTORE;
 import static org.apache.geode.distributed.ConfigurationProperties.SSL_TRUSTSTORE_PASSWORD;
-import static org.apache.geode.internal.Assert.assertTrue;
 import static org.apache.geode.util.test.TestUtil.getResourcePath;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TemporaryFolder;
 
 import org.apache.geode.management.internal.cli.i18n.CliStrings;
 import org.apache.geode.security.SecurableCommunicationChannels;
@@ -45,18 +60,6 @@ import org.apache.geode.test.dunit.rules.LocatorServerStartupRule;
 import org.apache.geode.test.dunit.rules.MemberVM;
 import org.apache.geode.test.junit.categories.DistributedTest;
 import org.apache.geode.test.junit.rules.serializable.SerializableTemporaryFolder;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TemporaryFolder;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.util.Properties;
 
 @Category(DistributedTest.class)
 public class ConnectToLocatorSSLDUnitTest {
@@ -66,7 +69,7 @@ public class ConnectToLocatorSSLDUnitTest {
 
   @Rule
   public RuleChain ruleChain =
-      RuleChain.outerRule(cleanupDUnitVMsRule).around(folder).around(lsRule);
+      RuleChain.outerRule(folder).around(cleanupDUnitVMsRule).around(lsRule);
 
   private File jks = null;
   private File securityPropsFile = null;
@@ -74,7 +77,7 @@ public class ConnectToLocatorSSLDUnitTest {
 
   @Before
   public void before() throws Exception {
-    this.jks = new File(getResourcePath(getClass(), "/ssl/trusted.keystore"));
+    jks = new File(getResourcePath(getClass(), "/ssl/trusted.keystore"));
     securityPropsFile = folder.newFile("security.properties");
     securityProps = new Properties();
   }
@@ -84,13 +87,13 @@ public class ConnectToLocatorSSLDUnitTest {
     securityPropsFile.delete();
   }
 
-  private void setUpLocatorAndConnect(Properties securityProps) throws Exception {
+  private void setUpLocatorAndConnect(Properties securityProps, final boolean useHttp)
+      throws Exception {
     MemberVM locator = lsRule.startLocatorVM(0, securityProps);
 
     // saving the securityProps to a file
     OutputStream out = new FileOutputStream(securityPropsFile);
     securityProps.store(out, null);
-
 
     /*
      * When using SSL, the GfshShellConnectionRule seems to leave behind state in the JVM that
@@ -98,14 +101,29 @@ public class ConnectToLocatorSSLDUnitTest {
      * together, the second and third tests will fail.) To avoid this issue, we connect to our
      * locator from a remote VM which is cleaned up by the CleanupDUnitVMsRule in between tests.
      */
-
-    final int locatorPort = locator.getPort();
     final String securityPropsFilePath = securityPropsFile.getCanonicalPath();
     Host.getHost(0).getVM(1).invoke(() -> {
       GfshShellConnectionRule gfshConnector = new GfshShellConnectionRule();
+
       try {
-        gfshConnector.connectAndVerify(locatorPort, GfshShellConnectionRule.PortType.locator,
-            CliStrings.CONNECT__SECURITY_PROPERTIES, securityPropsFilePath);
+        if (useHttp) {
+          // Our SSL certificate used for tests does not match the hostname "localhost"
+          HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+
+          final int httpPort = locator.getHttpPort();
+          gfshConnector.connectAndVerify(httpPort, GfshShellConnectionRule.PortType.http,
+              CliStrings.CONNECT__SECURITY_PROPERTIES, securityPropsFilePath,
+              CliStrings.CONNECT__USE_SSL, "true");
+        } else {
+          final int locatorPort = locator.getPort();
+          gfshConnector.connectAndVerify(locatorPort, GfshShellConnectionRule.PortType.locator,
+              CliStrings.CONNECT__SECURITY_PROPERTIES, securityPropsFilePath);
+        }
+
+        gfshConnector.executeAndVerifyCommand("list members");
+        gfshConnector.disconnect();
+      } catch (Exception e) {
+        e.printStackTrace();
       } finally {
         gfshConnector.close();
       }
@@ -113,21 +131,30 @@ public class ConnectToLocatorSSLDUnitTest {
   }
 
   @Test
-  public void testConnectToLocatorWithSSLJMX() throws Exception {
-    securityProps.setProperty(SSL_ENABLED_COMPONENTS, SecurableCommunicationChannels.JMX);
+  public void testConnectToLocatorOverJMX_withSSL() throws Exception {
+    setupSSLProperties();
+    setUpLocatorAndConnect(securityProps, false);
+  }
+
+  @Test
+  // Exposes GEODE-3097
+  public void testConnectToLocatorOverHTTP_withSSL() throws Exception {
+    setupSSLProperties();
+    setUpLocatorAndConnect(securityProps, true);
+  }
+
+  private void setupSSLProperties() throws Exception {
+    securityProps.setProperty(SSL_ENABLED_COMPONENTS, SecurableCommunicationChannels.ALL);
     securityProps.setProperty(SSL_KEYSTORE, jks.getCanonicalPath());
     securityProps.setProperty(SSL_KEYSTORE_PASSWORD, "password");
-    // securityProps.setProperty(SSL_KEYSTORE_TYPE, "JKS");
     securityProps.setProperty(SSL_TRUSTSTORE, jks.getCanonicalPath());
     securityProps.setProperty(SSL_TRUSTSTORE_PASSWORD, "password");
     securityProps.setProperty(SSL_PROTOCOLS, "TLSv1.2");
     securityProps.setProperty(SSL_CIPHERS, "any");
-
-    setUpLocatorAndConnect(securityProps);
   }
 
   @Test
-  public void testConnectToLocatorWithLegacyClusterSSL() throws Exception {
+  public void testConnectToLocatorOverJMX_withLegacyClusterSSL() throws Exception {
     securityProps.setProperty(CLUSTER_SSL_ENABLED, "true");
     securityProps.setProperty(CLUSTER_SSL_KEYSTORE, jks.getCanonicalPath());
     securityProps.setProperty(CLUSTER_SSL_KEYSTORE_PASSWORD, "password");
@@ -135,11 +162,11 @@ public class ConnectToLocatorSSLDUnitTest {
     securityProps.setProperty(CLUSTER_SSL_TRUSTSTORE, jks.getCanonicalPath());
     securityProps.setProperty(CLUSTER_SSL_TRUSTSTORE_PASSWORD, "password");
 
-    setUpLocatorAndConnect(securityProps);
+    setUpLocatorAndConnect(securityProps, false);
   }
 
   @Test
-  public void testConnectToLocatorWithLegacyJMXSSL() throws Exception {
+  public void testConnectToLocatorOverJMX_withLegacyJMXManagerSSL() throws Exception {
     securityProps.setProperty(JMX_MANAGER_SSL_ENABLED, "true");
     securityProps.setProperty(JMX_MANAGER_SSL_KEYSTORE, jks.getCanonicalPath());
     securityProps.setProperty(JMX_MANAGER_SSL_KEYSTORE_PASSWORD, "password");
@@ -147,7 +174,7 @@ public class ConnectToLocatorSSLDUnitTest {
     securityProps.setProperty(JMX_MANAGER_SSL_TRUSTSTORE, jks.getCanonicalPath());
     securityProps.setProperty(JMX_MANAGER_SSL_TRUSTSTORE_PASSWORD, "password");
 
-    setUpLocatorAndConnect(securityProps);
+    setUpLocatorAndConnect(securityProps, false);
   }
 
 }
