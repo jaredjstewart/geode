c70e3c2fd0bcce21ba5bfc3cf86d6d22c65b425f
Jared Stewart <jstewart@pivotal.io>
2017-06-20 11:08:14 -0700

    Add realgfsh test for ssl

--- geode-assembly/src/test/java/org/apache/geode/management/internal/cli/commands/StatusLocatorRealGfshTest.java
+++ geode-assembly/src/test/java/org/apache/geode/management/internal/cli/commands/StatusLocatorRealGfshTest.java
@@ -14,21 +14,65 @@
  */
 package org.apache.geode.management.internal.cli.commands;
 
+import static org.apache.geode.distributed.ConfigurationProperties.SSL_CIPHERS;
+import static org.apache.geode.distributed.ConfigurationProperties.SSL_ENABLED_COMPONENTS;
+import static org.apache.geode.distributed.ConfigurationProperties.SSL_KEYSTORE;
+import static org.apache.geode.distributed.ConfigurationProperties.SSL_KEYSTORE_PASSWORD;
+import static org.apache.geode.distributed.ConfigurationProperties.SSL_PROTOCOLS;
+import static org.apache.geode.distributed.ConfigurationProperties.SSL_TRUSTSTORE;
+import static org.apache.geode.distributed.ConfigurationProperties.SSL_TRUSTSTORE_PASSWORD;
+import static org.apache.geode.util.test.TestUtil.getResourcePath;
+
+import org.apache.geode.security.SecurableCommunicationChannels;
 import org.apache.geode.test.dunit.rules.gfsh.GfshRule;
 import org.apache.geode.test.dunit.rules.gfsh.GfshScript;
 import org.apache.geode.test.junit.categories.DistributedTest;
 import org.apache.geode.test.junit.categories.IntegrationTest;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 @Category(DistributedTest.class)
 public class StatusLocatorRealGfshTest {
   @Rule
   public GfshRule gfshRule = new GfshRule();
+
+  @ClassRule
+  public static  TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  private static String sslSecurityPropsFile;
+  private static String jksFile;
+
+  @BeforeClass
+  public static void setup() throws Exception {
+    File jks = new File(getResourcePath(StatusLocatorRealGfshTest.class, "/ssl/trusted.keystore"));
+
+    Properties securityProps = new Properties();
+    securityProps.setProperty(SSL_ENABLED_COMPONENTS, SecurableCommunicationChannels.ALL);
+    securityProps.setProperty(SSL_KEYSTORE, jks.getCanonicalPath());
+    securityProps.setProperty(SSL_KEYSTORE_PASSWORD, "password");
+    securityProps.setProperty(SSL_TRUSTSTORE, jks.getCanonicalPath());
+    securityProps.setProperty(SSL_TRUSTSTORE_PASSWORD, "password");
+    securityProps.setProperty(SSL_PROTOCOLS, "TLSv1.2");
+    securityProps.setProperty(SSL_CIPHERS, "any");
+
+    File  securityPropsFile = temporaryFolder.newFile("security.properties");
+    securityProps.store(new FileOutputStream(securityPropsFile), null);
+
+    sslSecurityPropsFile = securityPropsFile.getAbsolutePath();
+    jksFile = jks.getAbsolutePath();
+  }
 
   @Test
   public void statusLocatorSucceedsWhenConnected() throws Exception {
@@ -47,4 +91,25 @@ public class StatusLocatorRealGfshTest {
     gfshRule.execute(GfshScript.of("status locator --name=locator1")
         .awaitAtMost(1, TimeUnit.MINUTES).expectExitCode(1));
   }
+
+  @Test
+  public void sslOverJmx() {
+    gfshRule.execute(GfshScript.of("start locator --name=locator1 --security-properties-file=" + sslSecurityPropsFile).awaitAtMost(1, TimeUnit.MINUTES)
+        .expectExitCode(0));
+
+    gfshRule.execute(GfshScript.of("connect --locator=localhost[10334] --security-properties-file=" + sslSecurityPropsFile, "list members")
+        .awaitAtMost(1, TimeUnit.MINUTES).expectExitCode(0));
+  }
+
+  @Test
+  public void sslOverHttp() {
+    gfshRule.execute(GfshScript.of("start locator --name=locator1 --security-properties-file=" + sslSecurityPropsFile).awaitAtMost(1, TimeUnit.MINUTES)
+        .expectExitCode(0));
+
+    gfshRule.execute(GfshScript.of("connect --use-http=true --use-ssl=true --trust-store=/Users/jstewart/projects/open/geode-core/build/resources/test/ssl/trusted.keystore --trust-store-password=password --security-properties-file="+sslSecurityPropsFile, "list members")
+        .awaitAtMost(1, TimeUnit.MINUTES).expectExitCode(0));
+    gfshRule.execute(GfshScript.of("connect --use-http=true --use-ssl=true --trust-store=/Users/jstewart/projects/open/geode-core/build/resources/test/ssl/trusted.keystore --trust-store-password=password --key-store-password=password --key-store=/Users/jstewart/projects/open/geode-core/build/resources/test/ssl/trusted.keystore --security-properties-file="+sslSecurityPropsFile, "list members")
+        .awaitAtMost(1, TimeUnit.MINUTES).expectExitCode(0));
+  }
+
 }
