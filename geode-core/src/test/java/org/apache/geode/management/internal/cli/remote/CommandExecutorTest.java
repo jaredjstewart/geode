4946bb6cd14734c72da5058b164a4724db9e7c3b
Jinmei Liao <jiliao@pivotal.io>
2017-06-30 15:37:47 -0700

    added TestingConstructor annotation

--- geode-core/src/test/java/org/apache/geode/management/internal/cli/remote/CommandExecutorTest.java
+++ geode-core/src/test/java/org/apache/geode/management/internal/cli/remote/CommandExecutorTest.java
@@ -1,18 +1,16 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package org.apache.geode.management.internal.cli.remote;
@@ -35,7 +33,7 @@ public class CommandExecutorTest {
   Result result;
 
   @Before
-  public void before(){
+  public void before() {
     parseResult = mock(ParseResult.class);
     result = mock(Result.class);
     executor = new CommandExecutor();
@@ -51,7 +49,7 @@ public class CommandExecutorTest {
 
   @Test
   public void returnsResultAsExpected() throws Exception {
-    executor = new CommandExecutor(){
+    executor = new CommandExecutor() {
       protected Object invokeCommand(ParseResult parseResult) {
         return result;
       }
@@ -62,7 +60,7 @@ public class CommandExecutorTest {
 
   @Test
   public void testNullResult() throws Exception {
-    executor = new CommandExecutor(){
+    executor = new CommandExecutor() {
       protected Object invokeCommand(ParseResult parseResult) {
         return null;
       }
@@ -73,7 +71,7 @@ public class CommandExecutorTest {
 
   @Test
   public void anyRuntimeExceptionGetsCaught() throws Exception {
-    executor = new CommandExecutor(){
+    executor = new CommandExecutor() {
       protected Object invokeCommand(ParseResult parseResult) {
         throw new RuntimeException("my message here");
       }
@@ -84,12 +82,12 @@ public class CommandExecutorTest {
 
   @Test
   public void notAuthorizedExceptionGetsThrown() throws Exception {
-    executor = new CommandExecutor(){
+    executor = new CommandExecutor() {
       protected Object invokeCommand(ParseResult parseResult) {
         throw new NotAuthorizedException("Not Authorized");
       }
     };
-    assertThatThrownBy(()->executor.execute(parseResult))
+    assertThatThrownBy(() -> executor.execute(parseResult))
         .isInstanceOf(NotAuthorizedException.class);
 
   }
